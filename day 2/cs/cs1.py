1003076cbbf70a720edbd4c2ec48906027ec0c6c
Zuka6500 <katsaravazuka@gmail.com>
2026-02-06 23:06:54 +0400

    day 2

---
+++ day 2/cs/cs1.py
@@ -0,0 +1,5 @@
+word1 = "I"
+word2 = "Am"
+word3 = "Learning"
+word4 = "Python"
+print(word1+" "+word2+" "+word3+" "+word4)
---
+++ day 2/hw/HW2.py
@@ -0,0 +1,41 @@
+number1 = 5
+number2 = 10
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)   
+print(number1 / number2)    
+print(number1 ** number2)
+print(number1 // number2)
+print(number1 % number2)   
+
+
+birth_year = 2011
+current_year = 2026
+age = current_year - birth_year
+print(age)
+
+
+
+number1 = 5
+number2 = 10   
+number3 = 15
+number4 = 20
+number5 = 25
+print((number1 + number2 + number3 + number4 + number5) /4)
+
+# დეკლარაცია, ცვლადების გამოცხადებას ან შექმნას ნიშნავს.
+name = "zuka"
+
+#ინიციალიზაცია, ცვლადისთვის საწყის მნიშვნელობის მინიჭებას ნიშნავსს.
+age = 15 # name = "zuka"-ც არის ინიციალიზაიცია.
+
+#რეინიციალიზაცია, რაიმე ცვლადის მნიშვნელობის შეცვლას ნიშნავს.
+name = "Aleqsandre" # ინიციალიზაცია.
+name = "Saba" # რეინიციალიზაცია.
+
+
+#კონკატენაცია(concatenation) ნიშნავს ტექსტების (string-ების) შეერთებას.
+word1 = "I"
+word2 = "Am"
+word3 = "Zuka"
+print(word1 + " " + word2 + " " + word3)
