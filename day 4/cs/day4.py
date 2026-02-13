a1e2be11db9ee6b4a1d07e85ab7a3b618d9d85af
Zuka6500 <katsaravazuka@gmail.com>
2026-02-13 22:45:35 +0400

    day 4

---
+++ day 4/cs/day4.py
@@ -0,0 +1,10 @@
+user_name = "Zuka"
+user_last_name = "Katsarava"
+user_age =  15
+user_adress = "Ana Politkovskaya"
+user_hobby = "Playing Tennis"
+print(user_name)
+print(user_last_name)  
+print(user_age) 
+print(user_adress)  
+print(user_hobby)
---
+++ day 4/hw/hw4.py
@@ -0,0 +1,42 @@
+#Input ნიშნავს მონაცემების მიწოდებას პროგრამისთვის. 
+#ეს შეიძლება იყოს კლავიატურიდან, მაუსიდან ან ჯოისტიკიდან მოწოდებული მონაცემები.
+
+#Output არის პროგრამის მიერ დამუშავებული შედეგის ჩვენება ან გადაცემა.
+#ეს შეიძლება იყოს ეკრანზე ან მონიტორზე გამოსახული ტექსტი, გრაფიკა ან სხვა ფორმის ინფორმაცია.
+
+
+#1 
+user_name = input("enter your name: ")
+print("Hello,", user_name)
+#2
+user_age = input("enter your age: ")
+print("Your age is:", user_age)
+#3
+user_surname = input("enter your surname: ")
+print("Your surname is:", user_surname)
+#4
+user_hobby = input("enter your hobby: ")   
+print("Your hobby is:", user_hobby)
+#5
+user_height = input("enter your height: ")
+print("Your height is:", user_height)
+
+
+
+name = input("შეიყვანეთ თქვენი სახელი: ")
+surname = input("შეიყვანეთ თქვენი გვარი: ")
+address = input("შეიყვანეთ თქვენი მისამართი: ")
+hobby = input("შეიყვანეთ თქვენი ჰობი: ")
+fav_movie = input("შეიყვანეთ თქვენი საყვარელი ფილმი: ")
+print("გამარჯობა! მე ვარ " + name + " " + surname + 
+      ", ვცხოვრობ მისამართზე: " + address + 
+      ". ჩემი ჰობია " + hobby + 
+      " და ჩემი საყვარელი ფილმია " + fav_movie + ".")
+
+# Case Sensitive Language ნიშნავს, რომ პროგრამირების ენა
+# განასხვავებს დიდ (Uppercase) და პატარა (Lowercase) ასოებს.
+
+# snake_case არის ცვლადებისა და ფუნქციების სახელების წერის სტილი.
+# აქ სიტყვები გამოყოფილია "_" სიმბოლოთი,
+# რაც ვიზუალურად გავს გველის მოძრაობას,
+# ამიტომ ჰქვია "snake_case" (გველის სტილი).
