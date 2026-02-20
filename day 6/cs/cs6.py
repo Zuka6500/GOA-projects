c1f0cf87f5aa59912e35ac8a92630ffaf06e5abf
Zuka6500 <katsaravazuka@gmail.com>
2026-02-20 22:08:05 +0400

    day 6

---
+++ day 6/cs/cs6.py
@@ -0,0 +1,8 @@
+dog = input("enter your dog's name:")
+print("your dog's name is:", dog)
+print(type(dog))
+
+user_age = input("enter the year you were born:")
+current_year = 2026
+age = current_year - user_age
+print("you are", age, "years old")
